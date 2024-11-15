8fe76e32f0b3c958c06f49be083b6c42075b5d94
Adi Wahyudi <adi.whyd02@gmail.com>
2024-11-15 14:06:34 +0700

    test: add test configuration

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: "jest-expo",
+  testMatch: ["**/__tests__/**/*-test.[jt]s?(x)"],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.{ts,tsx,js,jsx}",
+    "!**/coverage/**",
+    "!**/node_modules/**",
+    "!**/babel.config.js",
+    "!**/expo-env.d.ts",
+    "!**/.expo/**",
+    "!**/jest.config.js",
+    "!**/.eslintrc.js",
+    "!**/constants/**",
+    "!**/scripts/**",
+    "!**/api/**",
+  ],
+};
