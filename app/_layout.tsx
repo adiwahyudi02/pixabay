ece21ecb8c8fef95f2b8025b9ece0fc1875a2b49
Adi Wahyudi <adi.whyd02@gmail.com>
2024-11-18 01:10:17 +0700

    feat: create RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,47 @@
+import "react-native-reanimated";
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { Provider } from "react-redux";
+import { store } from "@/store";
+import Toast from "react-native-toast-message";
+import { AuthProtect } from "@/components/commons/AuthProtect";
+import { LogBox } from "react-native";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+LogBox.ignoreAllLogs(true);
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded] = useFonts({
+    "Poppins-Bold": require("@/assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-Regular": require("@/assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Medium": require("@/assets/fonts/Poppins-Medium.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  useEffect(() => {
+    LogBox.ignoreAllLogs();
+  }, []);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <Provider store={store}>
+      <AuthProtect>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+        <Toast />
+      </AuthProtect>
+    </Provider>
+  );
 }
