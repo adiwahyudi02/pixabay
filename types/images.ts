f92027911d1b62cfd90e4ed9637c05bbcbf40a13
Adi Wahyudi <adi.whyd02@gmail.com>
2024-11-18 01:04:33 +0700

    feat: add images types

---
+++ types/images.ts
@@ -0,0 +1,32 @@
+export interface ImagesRes {
+  total: number;
+  totalHits: number;
+  hits: Image[];
+}
+
+export interface Image {
+  id: number;
+  pageURL: string;
+  type: string;
+  tags: string;
+  previewURL: string;
+  previewWidth: number;
+  previewHeight: number;
+  webformatURL: string;
+  webformatWidth: number;
+  webformatHeight: number;
+  largeImageURL: string;
+  imageWidth: number;
+  imageHeight: number;
+  imageSize: number;
+  views: number;
+  downloads: number;
+  collections: number;
+  likes: number;
+  comments: number;
+  user_id: number;
+  user: string;
+  userImageURL: string;
+}
+
+export interface Images extends Array<Image> {}
